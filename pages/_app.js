607ba39fe721e4e5a014cac489e8349bd9dcba93
dejosemario <josemariaodumodu@gmail.com>
2022-08-02 04:29:37 +0100

    setting the path ready, import fontawesome, moving icon to public root folder

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,7 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import "@fortawesome/fontawesome-svg-core/styles.css"; //import font awesome css
+import { config } from '@fortawesome/fontawesome-svg-core';
+config.autoAddCss = false; // Disable auto-adding of css by font awesome
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
